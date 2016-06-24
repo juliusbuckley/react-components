ae7296607bc7c3981a2fbccc871f1326dccdcdfd
juliusbuckley <juliusbuckley@gmail.com>
2016-06-24 12:08:28 -0700

    Completed prompts for react-components.

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 // TODO
+var GroceryList = (props) => (
+	<div>
+		<h1>Grocery List</h1>
+			<ul>
+				{props.items.map(item =>
+					<GroceryListItem item = {item} />
+				)}
+			</ul>
+	</div>
+);
+
+class GroceryListItem extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			done: false
+		};
+	}
+
+	onListItemHoverOver() {
+		this.setState({
+			done: true
+		});
+	}
+	
+	onListItemHoverOut() {
+		this.setState({
+			done: false
+		});
+	}
+
+	render() {
+		var style = {
+			fontWeight: this.state.done ? 'bold' : 'normal'
+		};
+
+		return (
+			<li style={style} onMouseOver={this.onListItemHoverOver.bind(this)} onMouseOut={this.onListItemHoverOut.bind(this)}>{this.props.item}</li>
+		);
+	}
+}
+
+ReactDOM.render(<GroceryList items = {['Eggs', 'Chicken']}/>, document.getElementById('app'));
